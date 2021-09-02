b81e2ac16107c3593015e69cfdb42af98522536b
AthulyaJA <77480834+AthulyaJA@users.noreply.github.com>
2021-09-02 17:31:10 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Feather } from '@expo/vector-icons';
+
 import {
   StyleSheet,
   Text,
@@ -10,22 +11,21 @@ import {
   Image,
   Alert,Linking
 } from 'react-native';
+import Register from './Register.js'
+import Modal from "react-native-modal";
 
-export default class LoginView extends Component {
+const App=()=> {
 
-  constructor(props) {
-    super(props);
-    state = {
-      email   : '',
-      password: '',
-    }
-  }
-
+ const[email,setEmail]=useState("")
+ const[password,setPassword]=useState("")
+ const [isModalVisible, setModalVisible] = useState(false);
   onClickListener = (viewId) => {
     Alert.alert("Alert", "Button pressed "+viewId);
   }
 
-  render() {
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
     return (
       
       <View style={styles.container}>
@@ -42,7 +42,7 @@ export default class LoginView extends Component {
               placeholder="Email"
               keyboardType="email-address"
               underlineColorAndroid='transparent'
-              onChangeText={(email) => this.setState({email})}/>
+              onChangeText={email}/>
               <Feather name="mail" size={24} style={styles.inputIcon} color="white" />
         </View>
         
@@ -52,15 +52,15 @@ export default class LoginView extends Component {
               placeholder="Password"
               secureTextEntry={true}
               underlineColorAndroid='transparent'
-              onChangeText={(password) => this.setState({password})}/>
+              onChangeText={password}/>
               <Feather name="lock" size={24} color="white" style={styles.inputIcon} />
         </View>
 
-        <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.onClickListener('login')}>
+        <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => onClickListener('login')}>
           <Text style={styles.loginText}>Login</Text>
         </TouchableHighlight>
 
-        <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('restore_password')}>
+        <TouchableHighlight style={styles.buttonContainer} onPress={() => onClickListener('restore_password')}>
             <Text style={styles.fgpass}>Forgot your password?</Text>
         </TouchableHighlight>
 
@@ -73,19 +73,55 @@ export default class LoginView extends Component {
    <Text > <Text style={styles.account} > Don't have an account ?</Text> <Text style={styles.create}>Create new now?</Text> </Text>
       </View>
       <View style={styles.terms}>
-        <Text style={styles.account}>By your are logining in,you are agree</Text><Text style={styles.account}> with Wyld's <Text style={{color: 'blue'}} onPress={() => Linking.openURL('http://google.com')}>Terms & Conditions</Text></Text>
+        <Text style={styles.account}>By your are logining in,you are agree</Text><Text style={styles.account}> with Wyld's <Text style={{color: 'blue'}} onPress={toggleModal}>Terms & Conditions</Text></Text>
       </View>
-      
+      <View style={styles.modal}>
+      <Modal isVisible={isModalVisible}>
+        <View  style={styles.modal}>
+          <Text style={styles.modalterms}>Terms & Conditions</Text>
+                            <Text style={styles.modalcontent}> 
+                            <Text>while still providing a simple API
+                                  while still providing a simple API
+                                  while still providing a simpleAPIwhile still providing a simple APIwhile still providing a simple API
+                                  while still providing a simple APIdvfkvjfvfdkgfdgjfdg
+                                  </Text>
+                                  while still providing a simple APIAPIwhile still providing a simple APIwhile still providing a simple API
+                                  while still providing a simple APIjscdkfjdsfidjigvfdjvfdvf
+                                 while still providing a simple APIAPIwhile still providing a simple APIwhile still providing a simple API
+                                  still providing a simple APIdjfkdsfjdkvfdjvkfdgjdfkgfjgfgfg
+                                 vbvvbvbwhile still providing a simple APIAPIwhile still providing a simple APIwhile still providing a simple API
+                                gghggbbbb while still providing a simple APIbccjjjjjjjcdkijdfidfjsgifig
+                                 while still providing a simple APIdfjsdfkjdvdkcdjkcdcjdjcjdjfjdjjjdj
+                                while still providing a simple APIdcsdkdjvdfvjdjdssssssssssssjjsjsjajdsjdjsjdjsjj
+                                APIwhile still providing a simple APIwhile still providing a simple API
+                               while still providing a simple APIAPIwhile still providing a simple APIwhile still providing a simple API
+                               while still providing a simple APIdcjdvjdfdvjfkvf
+                               while still providing a simple APIdckdsfksdfkdofcdk
+                              while still providing a simple APIAPIwhile still providing a simple APIwhile still providing a simple API
+                              while still providing a simple APIdkfdsofksdfod
+                              while still providing a simple APImfdslfksofkdosfsofd
+                              APIwhile still providing a simple APIwhile still providing a simple API
+                              while still providing a simple APIdkasodkosadsdks
+                              while still providing a simple APIAPIwhile still providing a simple APIwhile still providing a simple API
+                              while still providing a simple APIksadasdkaosdksiodai
+                              while still providing a simple APIajdadaskdsjdsjdj
+                              APIwhile still providing a simple APIwhile still providing a simple API
+                              while still providing a simple API
+                              while still providing a simple API
+                            </Text>
+
+          <Button title="Hide modal" style={styles.modalbut} onPress={toggleModal} />
+        </View>
+      </Modal>
+      </View>
       </View>
       
     );
   }
-}
 
 
-function Register(){
-  
-}
+export default App;
+
 
 const styles = StyleSheet.create({
   container: {
@@ -169,6 +205,20 @@ color:"#ffffff"
  },terms:{
    marginTop:100,
    justifyContent: 'center',
+ },
+ modalbut:{
+  backgroundColor: "#ffff00",
+ },
+ modal:{
+  backgroundColor:"#000000"
+ },
+ modalcontent:{
+   color:"#ffffff",
+   marginTop:20
+ },
+ modalterms:{
+   fontSize:30,
+   color:"#ffffff"
  }
 });
 
